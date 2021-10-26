ef3cfbc7206dfbd3f81bf9a243fcb953567f90f3
Linna He <z5117757@ad.unsw.edu.au>
2021-10-26 16:51:47 +1100

    hello

--- Display/main.cpp
+++ Display/main.cpp
@@ -54,9 +54,6 @@ void mouse(int button, int state, int x, int y);
 void dragged(int x, int y);
 void motion(int x, int y);
 
-//void plotLaser();
-//void plotGPS();
-
 using namespace std;
 using namespace scos;
 
